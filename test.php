7012b0eeffd7f22291bb1cb27a338983a274ea86
xkaxha123 <xkaxha123@naver.com>
2016-09-01 10:26:43 +0900

    heap & ass lab

---
+++ test.php
@@ -0,0 +1,22 @@
+<?php
+    //phpinfo();
+?>
+<!doctype html>
+<html lang = "en">
+    <<head>
+        <meta charset = "utf-8">
+    <title>엉덩이 연구 논문</title>
+    </head>
+    <body>
+        <ul>
+            <li>1th</li>
+                <h1>김햄준 엉덩이는 퍼스트 힙이다.</h1>
+            <li>second</li>
+                <h2>점승리 엉덩이는 세컨드 힙이다.</h1>
+            <li>3rd</li>
+                <h3>이륙구 엉덩이는 써드   힙이다.</h3>
+            <li>4th</li>
+                <h4>정태웅은 민간인이다.</h4>
+        </ul>
+    </body>
+</html>
