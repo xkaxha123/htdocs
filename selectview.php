2bf3fb905926a2e2dcb059ec3e3b51f9208af4d7
xkaxha123 <xkaxha123@naver.com>
2016-09-08 11:01:46 +0900

    added yo

--- selectview.php
+++ selectview.php
@@ -1,15 +1,16 @@
 <?php
     $location = $_POST["location"];
     $birth = $_POST["year"];
-    if($location == "unselected"){
-        print "지역을 입력하세요.<br>";
+    $yo = 2016 - $birth;
+    if($location != "unselected"){
+        print "지역은 ".$location."입니다.<br>";
     }
-    else if(isset($location)){
-        print "지역은".$location."입니다.<br>";
+    else{
+        print "지역을 입력하세요.<br>";
     }
     
     if(isset($birth)){
-        print "당신은".$birth."년생입니다.<br>";
+        print "당신은 ".$birth."년생 ".$yo."세입니다.<br>";
     }
     else{
         print "출생년도를 선택해 주세요.<br>";
