e92c462ba90adc78c0c93b0282f88413a65e6d37
xkaxha123 <xkaxha123@naver.com>
2016-09-09 11:26:54 +0900

    studied mysql

---
+++ mysql.php
@@ -0,0 +1,24 @@
+<html>
+    <head>
+        <title></title>
+    </head>
+    <body>
+        <?php
+            /*  mysql 함수
+            $con = mysql_connect('localhost', 'root', '') or die("접속 실패");
+            print "접속 성공";
+            mysql_select_db('php');
+            mysql_close($con);
+            */
+            
+            /*  mysqli 함수
+            $mysqli = new mysqli('localhost', 'root', '', 'php');
+            if($mysqli -> connect_error){die("접속 실패");}
+            print "접속 성공";
+            $mysqli -> close();
+            */
+        
+            
+        ?>
+    </body>
+</html>
